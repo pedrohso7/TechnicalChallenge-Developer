532a695573d1d58e548ee3baa84931c03e143085
Pedro Oliveira <pholiveira1998@hotmail.com>
2022-09-16 17:26:54 -0300

    refactor(repository): refactor user repository names

--- src/repositories/user_repository/user_repository.ts
+++ src/repositories/user_repository/user_repository.ts
@@ -1,10 +1,14 @@
 import { User } from "../../app/entities/user";
 import { UserDatabaseModel } from "../../database/schemas/user_database_model";
-import { UserRepository } from "./user_repository_interface";
+import { UserRepositoryInterface } from "./user_repository_interface";
 
-export class InMemoryUserRepository implements UserRepository{
+export class UserRepository implements UserRepositoryInterface {
     async create(user: User): Promise<User | null> {
         const userDB = new UserDatabaseModel();
         return userDB.databaseModel().create(user);
+    }
+
+    async getUsersFromExternalApi(): Promise<User[]>{
+        return [];
     }
 }
--- src/repositories/user_repository/user_repository_interface.ts
+++ src/repositories/user_repository/user_repository_interface.ts
@@ -1,5 +1,7 @@
 import { User } from "../../app/entities/user";
 
-export interface UserRepository{
+export interface UserRepositoryInterface{
     create(user: User): Promise<User | null>;
+    // getUsersFromDatabase(): Promise<User[]>;
+    getUsersFromExternalApi(): Promise<User[]>;
 }
