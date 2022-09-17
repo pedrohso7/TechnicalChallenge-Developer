2fb1b5d0ced3e67a5c4c5775082f6370b94fa386
Pedro Oliveira <pholiveira1998@hotmail.com>
2022-09-17 18:19:50 -0300

    feat(users): add the method will get all users from link api

--- src/repositories/user_repository/user_repository.ts
+++ src/repositories/user_repository/user_repository.ts
@@ -1,14 +1,30 @@
 import { User } from "../../app/entities/user";
 import { UserDatabaseModel } from "../../database/schemas/user_database_model";
 import { UserRepositoryInterface } from "./user_repository_interface";
+import { ApiLinkAxios } from "../api_link_axios";
 
 export class UserRepository implements UserRepositoryInterface {
     async create(user: User): Promise<User | null> {
-        const userDB = new UserDatabaseModel();
-        return userDB.databaseModel().create(user);
+        try{
+            const userDB = new UserDatabaseModel();
+            return userDB.databaseModel().create(user);
+        } catch (error){
+            console.log(error);
+            throw error;
+        }    
     }
 
     async getUsersFromExternalApi(): Promise<User[]>{
-        return [];
+        try{
+            const api = new ApiLinkAxios();
+
+            let response = await api.axios.get('users');
+            console.log(response);
+            return [];
+        } catch (error){
+            console.log(error);
+            throw error;
+        }    
+        
     }
 }
